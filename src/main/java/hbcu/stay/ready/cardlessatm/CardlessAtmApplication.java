f755ff310494a7957c2a2a6da3bd88218501f98f
Caleb <cavinesscaleb@gmail.com>
2019-08-05 15:16:08 -0400

    Let's Start :)

---
+++ src/main/java/hbcu/stay/ready/cardlessatm/CardlessAtmApplication.java
@@ -0,0 +1,13 @@
+package hbcu.stay.ready.cardlessatm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardlessAtmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardlessAtmApplication.class, args);
+	}
+
+}
